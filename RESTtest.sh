7951c4e25f1db13e9493abb6cf863b04063ff875
Mark Lerner <g.um104.w@gmail.com>
2013-07-05 17:11:21 -0700

    Added .sh file for testing Apex REST API endpoints on Salesforce.com. Giving this proper credentials and the URL mappings for your endponts will run simple tests to just check that they work. This is good for integration testing.

---
+++ RESTtest.sh
@@ -0,0 +1,67 @@
+# Credential fields. Personalize these.
+USERNAME="username"
+PASSWORD="password"
+SECURITYTOKEN="securityToken123"
+CLIENTID="someSuperLongClientId1234567890"
+CLIENTSECRET="clientsecret12345"
+
+# Function for parsing the Instance URL and Access Token, courtesy of
+# https://gist.github.com/cjus/1047794
+function jsonval {
+    temp=$(echo $RESPONSE | sed 's/\\\\\//\//g' | sed 's/[{}]//g' | awk -v k="text" '{n=split($0,a,","); for (i=1; i<=n; i++) print a[i]}' | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' | grep -w $PROP| sed -e 's/^ *//g' -e 's/ *$//g')
+    echo ${temp##*|}
+}
+
+echo
+echo 'Be sure to change the username, password, security token, client ID, and client secret before running!'
+echo 'See https://na11.salesforce.com/help/doc/en/user_security_token.htm for more info on the security token.'
+echo 'See http://www.salesforce.com/us/developer/docs/api_rest/Content/quickstart_oauth.htm for info on the clientId and client secret.'
+echo
+
+# Attempt to authenticate
+RESPONSE=`curl --silent -S https://login.salesforce.com/services/oauth2/token -d "grant_type=password" -d "client_id=$CLIENTID" -d "client_secret=$CLIENTSECRET" -d "username=$USERNAME" -d "password=$PASSWORD$SECURITYTOKEN"`
+
+# check for errors
+PROP="error"
+ERROR=$(jsonval)
+if [ "$ERROR" != "" ]; then
+    PROP="error_description"
+    ERROR_DESCRIPTION=$(jsonval)
+    echo "AUTHENTICATION ERROR ENCOUNTERED"
+    echo "ERROR: " $ERROR
+    echo "ERROR_DESCRIPTION: " $ERROR_DESCRIPTION; echo
+    echo "FULL ERROR MESSAGE: " $RESPONSE; echo
+    echo "Your credentials may be invalid -- check them again!"; echo
+    exit
+else
+    echo "AUTHENTICATION SUCCESSFUL"; echo
+fi
+
+
+# Retrieve the Access Token
+PROP="access_token"
+ACCESS_TOKEN=$(jsonval)
+
+# Retrieve the Instance URL
+PROP="instance_url"
+INSTANCE_URL=$(jsonval)
+
+# BEGIN TESTS
+echo "----------------------"
+echo "Test AppsRestResource"
+echo "----------------------"
+curl $INSTANCE_URL/services/apexrest/get_apps -H 'Authorization: Bearer '$ACCESS_TOKEN
+echo
+echo "----------------------"
+echo
+echo "----------------------"
+echo "Test SessionRestResource"
+echo "----------------------"
+curl $INSTANCE_URL/services/apexrest/post_upload/ -H 'Authorization: Bearer '$ACCESS_TOKEN -H 'Content-Type: application/json' -d @session.json
+echo
+echo "----------------------"
+
+
+echo "Tests Done"
+echo
+echo
